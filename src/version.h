273c6f2e49f1cf90a7a6d6f0925814dc15b83417
TokiZeng <137824202+TokiZeng@users.noreply.github.com>
2023-07-24 00:59:02 +0800

    Add files via upload

---
+++ src/version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+extern const char* const TOKICO_VERSION_TAG;
+extern const char* const TOKICO_VERSION;
+extern const char* const TOKICO_RELEASE_NAME;
+extern const char* const TOKICO_VERSION_FULL;
+extern const bool TOKICO_VERSION_IS_RELEASE;
